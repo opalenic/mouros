4efadc578e4b8c6e084a4d4f8859910b9970a3e4
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2015-08-02 21:51:14 +0200

    Moved common.h from the common-stm32 repository.

---
+++ include/mouros/common.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ *
+ * Common macros that don't fit anywhere else.
+ *
+ */
+
+#ifndef MOUROS_COMMON_H_
+#define MOUROS_COMMON_H_
+
+/**
+ * Returns the size of the array in the argument.
+ */
+#define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x[0])))
+
+
+#endif /* MOUROS_COMMON_H_ */
