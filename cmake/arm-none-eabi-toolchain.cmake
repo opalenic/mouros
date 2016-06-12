4c167ebcf032486f8c17867b0428f7e860e9b6f0
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2016-06-12 17:03:28 +0200

    Moved the setting of MCU and FPU architecture into the toolchain file.

--- cmake/arm-none-eabi-toolchain.cmake
+++ cmake/arm-none-eabi-toolchain.cmake
@@ -9,3 +9,12 @@ set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_SYSTEM_VERSION "Generic")
 
 set(CMAKE_SYSROOT "/usr/${TARGET_TRIPLET}")
+
+
+if(CHIP_FAMILY STREQUAL "STM32F0")
+    set(CMAKE_C_FLAGS "-mcpu=cortex-m0 -msoft-float -mthumb" CACHE STRING "cflags")
+elseif(CHIP_FAMILY STREQUAL "STM32F4")
+    set(CMAKE_C_FLAGS "-mcpu=cortex-m4f -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb" CACHE STRING "cflags")
+else()
+    message(FATAL_ERROR "Unknown CHIP_FAMILY: ${CHIP_FAMILY}\nAllowed families are: STM32F0, STM32F4")
+endif()
